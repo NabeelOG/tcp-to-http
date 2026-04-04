f99c5c9a8d40400f9cfa11acab796ee53a4e6135
NabeelOG <nmuhammed851@gmail.com>
2026-04-04 02:27:02 +0000

    Convert 6-HTTP-Body from submodule to normal folder

---
+++ 6-HTTP-Body/internal/headers/headers_test.go
@@ -0,0 +1,53 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHeaderParse(t *testing.T) {
+	// Test: Valid single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\nFooFoo:    barbar    \r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	host, ok := headers.Get("Host")
+	assert.True(t, ok)
+	assert.Equal(t, "localhost:42069", host)
+
+	foofoo, ok := headers.Get("FooFoo")
+	assert.True(t, ok)
+	assert.Equal(t, "barbar", foofoo)
+
+	_, ok = headers.Get("missingKey")
+	assert.False(t, ok)
+	assert.Equal(t, 48, n)
+	assert.True(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	headers = NewHeaders()
+	data = []byte("Host: localhost:42069\r\nHost: localhost:42069\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	host, ok = headers.Get("HOST")
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069, localhost:42069", host)
+	assert.False(t, done)
+}
